daec9052fb1261809bdcb1c51496117a015cce8e
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-03-17 15:53:45 +0100

    Seperate function for plotting

--- notebooks/py_module/utils.py
+++ notebooks/py_module/utils.py
@@ -50,7 +50,7 @@ def create_hulls(tweets, clustering_algo, coords):
     return hulls
 
 
-def plot_space_time_cube(tweets):
+def plot_space_time_cube(tweets, eps1_terrain, eps2):
     # Create figure with 3D projected axis
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(5, 5))
 
@@ -114,10 +114,117 @@ def plot_space_time_cube(tweets):
     # Save fig
     plt.savefig("st_dbscan.png", dpi=300, bbox_inches="tight")
     plt.savefig(
-        f"./images/st_dbscan/st_dbscan_3d.png",
+        f"./images/st_dbscan/st_dbscan_3d_eps1_{eps1_terrain / 1_000:.0f}_km_eps2_{int(eps2 / 60)}_min.png",
         dpi=300,
         facecolor=ax.get_figure().get_facecolor(),
         edgecolor="none",
     )
 
     plt.show()
+
+
+def plot_hulls(tweets, hulls, eps1_terrain, eps2, min_samples):
+    # Read France geojson
+    basemap = gpd.read_file(filename="../data/france.geojson")
+    basemap = basemap.to_crs("EPSG:2154")
+
+    fig2, base = plt.subplots(figsize=(6, 6))
+    # Plot base map
+    basemap.plot(ax=base, color="#F2E7DC", figsize=(7, 7), linewidth=0)
+    base.set_facecolor("#818274")
+    base.get_figure().patch.set_facecolor("#22272e")
+
+    # Plot tweets with color based on cluster
+    for cluster in tweets.index.get_level_values(0).unique():
+        # Number of tweets in cluster
+        num_tweets = len(tweets[tweets.index.get_level_values(0) == cluster])
+        # Check whether cluster id exists in hulls
+        if cluster in hulls.index.values or cluster == -1:
+            print("Cluster {}: {} tweets".format(cluster, num_tweets))
+            tweets[tweets.index.get_level_values(0) == cluster].plot(
+                ax=base,
+                markersize=6 if cluster == -1 else 10,
+                label="Noise" if cluster == -1 else "Cluster {}".format(cluster),
+                marker="x" if cluster != -1 else "+",
+                color="#161819" if cluster == -1 else "C{}".format(cluster),
+                linewidth=0.5 if cluster == -1 else 0.8,
+            )
+
+    for cluster in hulls.index:
+        hulls[hulls.index == cluster].plot(
+            ax=base,
+            alpha=0.3,
+            color="C{}".format(cluster),
+            edgecolor="C{}".format(cluster),
+            linewidth=1,
+        )
+
+    # Set title
+    base.set_title(
+        "ST-DBSCAN Clustering of Teil Quake Tweets",
+        fontsize=16,
+        weight="medium",
+        color="white",
+        pad=10,
+    )
+
+    # Set legend
+    base.legend(
+        loc="best",
+        fontsize=9,
+        labelcolor="#22272e",
+        markerscale=2,
+        facecolor="white",
+        title_fontsize=14,
+        fancybox=False,
+        framealpha=1,
+        edgecolor="#22272e",
+    )
+    # bbox_to_anchor=(1.23, 1.01
+
+    # Add grid
+    base.grid(color="#161819", linestyle="-", linewidth=0.2, alpha=0.3)
+
+    # Change tick labels
+    m2km = lambda x, _: f"{x/1000:g}"
+    base.xaxis.set_major_formatter(m2km)
+    base.yaxis.set_major_formatter(m2km)
+
+    # Set axis labels
+    base.set_xlabel("X (km)", fontsize=7, labelpad=5, color="white")
+    base.set_ylabel("Y (km)", fontsize=7, labelpad=5, color="white")
+
+    # Set axis ticks color
+    base.tick_params(axis="x", colors="white")
+    base.tick_params(axis="y", colors="white")
+
+    textstr = "\n".join(
+        (
+            r"$eps1=%.0f$ km" % (eps1_terrain / 1_000,),
+            r"$eps2=%d$ min" % (eps2,),
+            r"$min_{samples}=%d$ tweets" % (min_samples,),
+        )
+    )
+
+    # these are matplotlib.patch.Patch properties
+    props = dict(facecolor="white", edgecolor="#22272e")
+
+    # place a text box in upper left in axes coords
+    base.text(
+        0.02,
+        0.98,
+        textstr,
+        transform=base.transAxes,
+        fontsize=8,
+        verticalalignment="top",
+        bbox=props,
+        color="#22272e",
+    )
+    # Save figures
+    base.get_figure().savefig(
+        f"./images/st_dbscan/st_dbscan_eps1_{eps1_terrain / 1_000:.0f}_km_eps2_{int(eps2 / 60)}_min.png",
+        dpi=300,
+        bbox_inches="tight",
+        facecolor=base.get_figure().get_facecolor(),
+        edgecolor="none",
+    )
